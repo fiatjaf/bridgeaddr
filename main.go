45f9fcd5ffd3f56186508e714764809448676e6c
fiatjaf <fiatjaf@alhur.es>
2021-08-01 14:55:25 -0300

    render README.md as the landing page.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -22,6 +25,9 @@ var s Settings
 var router = mux.NewRouter()
 var log = zerolog.New(os.Stderr).Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
+//go:embed README.md
+var readme string
+
 func main() {
 	err = envconfig.Process("", &s)
 	if err != nil {
@@ -31,7 +37,11 @@ func main() {
 	router.Path("/.well-known/lnurlp/{username}").Methods("GET").HandlerFunc(lnurlParams)
 	router.Path("/lnurl/{username}").Methods("GET").HandlerFunc(lnurlValues)
 
-	// router.Host(strings.Split(s.ServiceURL, "://")[1]).PathPrefix("/").Handler()
+	router.Host(strings.Split(s.ServiceURL, "://")[1]).PathPrefix("/").HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, readme+`<style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style></script><script src="https://casual-effects.com/markdeep/latest/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>`)
+		},
+	)
 
 	srv := &http.Server{
 		Handler:      router,
